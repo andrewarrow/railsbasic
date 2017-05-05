1cb46982287ae9464af19e3d07c92dafc270f84e
A Arrow <oneone@gmail.com>
2017-05-05 04:18:27 +0000

    mm

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,17 @@ class UsersController < ApplicationController
   def index
   end
   def new
+    set_flavor
+  end
+
+  def create
+    set_flavor
+    flash[:notice] = 'Check your email for password.'
+    redirect_to '/sessions/new'
+  end
+
+  def set_flavor
+    @flavor = 'a'
+    @flavor = 'b' if params[:flavor] == 'b'
   end
 end
