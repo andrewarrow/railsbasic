5da2dbb9169bf0e713c55c0a53ed51a740b9fb29
A Arrow <oneone@gmail.com>
2017-05-05 03:54:53 +0000

    m

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,15 @@
 class SessionsController < ApplicationController
   def new
+    @forgot = params[:forgot].to_i
   end
+
   def create
+    if params[:forgot].to_i == 1
+      flash[:notice] = 'Email sent.'
+      redirect_to '/'
+      return
+    end
+
     session[:user_id] = 1
     redirect_to '/'
   end
