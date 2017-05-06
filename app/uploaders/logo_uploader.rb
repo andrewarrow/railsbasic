521b5c582d7d300192e0d0c96e3eac62faf987e4
A Arrow <oneone@gmail.com>
2017-05-06 00:47:46 +0000

    png

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -4,12 +4,10 @@ class LogoUploader < CarrierWave::Uploader::Base
 
   storage :file
 
-  convert :png
   process :resize_to_limit => [1024,1024]
 
   version :thumb do
     process :resize_to_fill => [120, 120]
-    convert :png
   end
 
 end
