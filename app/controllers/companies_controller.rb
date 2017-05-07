e4481e45b0d2ae608cccb79bc412a714dce70a6a
A Arrow <oneone@gmail.com>
2017-05-07 02:24:38 +0000

    edit

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,12 +3,33 @@ class CompaniesController < ApplicationController
   def show
     @item = Company.where(slug: params[:id]).first
   end
+  def edit
+    @item = Company.where(slug: params[:id]).first
+  end
 
   def index
     @items = Company.all.order('created_at desc').limit(100)
   end
 
   def new
+  end
+
+  def update
+
+    @item = Company.where(slug: params[:id]).first
+    if @item.creator_id != current_user.id
+      return
+    end
+
+    @item.update_attributes(name: params[:name], 
+                            description: params[:description])
+    if params[:logo]
+      @item.logo = params[:logo]
+      @item.save
+    end
+
+    flash[:notice] = 'Changes saved.'
+    redirect_to '/companies/' + @item.slug + '/edit'
   end
 
   def create
